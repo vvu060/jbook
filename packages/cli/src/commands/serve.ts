eb1d00d0575e45b06bb7b4bfeb9773cb0649d03a
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-21 11:28:09 +0530

    add basic error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -10,7 +10,15 @@ export const serveCommand = new Command()
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
       await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
     } catch (err) {
-        console.log("Here is the error", err.message);
+        if(err.code === 'EADDRINUSE'){
+          console.error('Port is in use. Try running on a different port.');
+        } else {
+          console.log("Here is the error", err.message);
+        }
+        process.exit(1);
     }
   });
